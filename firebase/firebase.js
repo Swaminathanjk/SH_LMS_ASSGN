4668ca753de3c253d3880ffa4d30f25405419eb9
J K Swaminathan <82333513+Swaminathanjk@users.noreply.github.com>
2025-09-23 18:20:18 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvGx8m__9y9cEy3x7aDkrhAGUMnUzAPhQ",
-  authDomain: "lms-assgn.firebaseapp.com",
-  projectId: "lms-assgn",
-  storageBucket: "lms-assgn.firebasestorage.app",
-  messagingSenderId: "548810883263",
-  appId: "1:548810883263:web:513d4860578db11c10860a",
-  measurementId: "G-7K1DW2VJM6",
+  // your Config from Firebase
 };
 
 const app = initializeApp(firebaseConfig);
