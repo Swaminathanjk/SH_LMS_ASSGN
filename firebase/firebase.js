8b645368510833552331b73403068a67d11cf277
J K Swaminathan <82333513+Swaminathanjk@users.noreply.github.com>
2025-09-24 06:45:19 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-<<<<<<< HEAD
+
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -10,9 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-=======
-  // your Config from Firebase
->>>>>>> 4668ca753de3c253d3880ffa4d30f25405419eb9
+
 };
 
 const app = initializeApp(firebaseConfig);
